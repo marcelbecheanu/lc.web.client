f1be67c781042a602b60cd57298e52d6f5a205fe
Marcel Becheanu <marcel.becheanu@outlook.pt>
2022-10-18 23:25:22 +0100

    Improve landing page

--- src/components/Technologies/index.jsx
+++ src/components/Technologies/index.jsx
@@ -1,11 +1,17 @@
 import { useRef } from 'react';
 import { Rerousel } from 'rerousel';
-
+ 
 import styled from 'styled-components';
 
 function Technologies() {
     const ref = useRef(null);
   
+
+    const Wrapper = styled.div`
+      width: 100%;
+      height: 50%;
+    `
+
     const Item = styled.div`
       display: flex;
       justify-content: center;
@@ -24,13 +30,16 @@ function Technologies() {
     `;
   
     return (
-      <Rerousel itemRef={ref}>
-        <Item ref={ref}>1</Item>
-        <Item>2</Item>
-        <Item>3</Item>
-        <Item>4</Item>
-        <Item>5</Item>
-      </Rerousel>
+      <Wrapper>
+        <h1></h1>
+        <Rerousel itemRef={ref}>
+          <Item ref={ref}>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+          <Item>4</Item>
+          <Item>5</Item>
+        </Rerousel>
+      </Wrapper>
     );
   }
 
